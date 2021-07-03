865de1bd8f60aef06662965b718ad3c92dc8b199
Nguyen Ngoc Minh <nnminh432@gmail.com>
2021-07-03 14:49:23 +0700

    ok

---
+++ JS/index.js
@@ -0,0 +1,157 @@
+let a = document.querySelector("#list-image-first");
+function over(){
+    
+    document.querySelector("#add-card").style.display ="block";
+    document.querySelector("#add-card").style.transition="5s";
+    document.querySelector(".list-group").style.display="block";
+}
+function out(){
+    document.querySelector("#add-card").style.display ="none";
+    document.querySelector(".list-group").style.display="none";
+}
+function overFirst(){
+   
+    document.querySelector("#add-card-1").style.display ="block";
+ document.querySelector(".list-group-1").style.display="block";
+}
+   
+function outFirst(){
+    document.querySelector("#add-card-1").style.display ="none";
+    document.querySelector(".list-group-1").style.display="none";
+}
+function overSc(){
+   
+    document.querySelector("#add-card-2").style.display ="block";
+ document.querySelector(".list-group-2").style.display="block";
+}
+   
+function outSc(){
+    document.querySelector("#add-card-2").style.display ="none";
+    document.querySelector(".list-group-2").style.display="none";
+}
+function overTh(){
+   
+    document.querySelector("#add-card-3").style.display ="block";
+ document.querySelector(".list-group-3").style.display="block";
+}
+   
+function outTh(){
+    document.querySelector("#add-card-3").style.display ="none";
+    document.querySelector(".list-group-3").style.display="none";
+}
+function overF(){
+   
+    document.querySelector("#add-card-4").style.display ="block";
+ document.querySelector(".list-group-4").style.display="block";
+}
+   
+function outF(){
+    document.querySelector("#add-card-4").style.display ="none";
+    document.querySelector(".list-group-4").style.display="none";
+}
+function overFif(){
+   
+    document.querySelector("#add-card-5").style.display ="block";
+ document.querySelector(".list-group-5").style.display="block";
+}
+   
+function outFif(){
+    document.querySelector("#add-card-5").style.display ="none";
+    document.querySelector(".list-group-5").style.display="none";
+}
+function overSix(){
+   
+    document.querySelector("#add-card-6").style.display ="block";
+ document.querySelector(".list-group-6").style.display="block";
+}
+   
+function outSix(){
+    document.querySelector("#add-card-6").style.display ="none";
+    document.querySelector(".list-group-6").style.display="none";
+}
+function overSeven(){
+   
+    document.querySelector("#add-card-7").style.display ="block";
+ document.querySelector(".list-group-7").style.display="block";
+}
+   
+function outSeven(){
+    document.querySelector("#add-card-7").style.display ="none";
+    document.querySelector(".list-group-7").style.display="none";
+}
+function overE(){
+   
+    document.querySelector("#add-card-8").style.display ="block";
+ document.querySelector(".list-group-8").style.display="block";
+}
+   
+function outE(){
+    document.querySelector("#add-card-8").style.display ="none";
+    document.querySelector(".list-group-8").style.display="none";
+}
+function overN(){
+   
+    document.querySelector("#add-card-9").style.display ="block";
+ document.querySelector(".list-group-9").style.display="block";
+}
+   
+function outN(){
+    document.querySelector("#add-card-9").style.display ="none";
+    document.querySelector(".list-group-9").style.display="none";
+}
+function overT(){
+   
+    document.querySelector("#add-card-10").style.display ="block";
+ document.querySelector(".list-group-10").style.display="block";
+}
+   
+function outT(){
+    document.querySelector("#add-card-10").style.display ="none";
+    document.querySelector(".list-group-10").style.display="none";
+}
+function overTW(){
+   
+    document.querySelector("#add-card-11").style.display ="block";
+ document.querySelector(".list-group-11").style.display="block";
+}
+   
+function outTW(){
+    document.querySelector("#add-card-11").style.display ="none";
+    document.querySelector(".list-group-11").style.display="none";
+}
+function overTWL(){
+   
+    document.querySelector("#add-card-12").style.display ="block";
+ document.querySelector(".list-group-12").style.display="block";
+}
+   
+function outTWL(){
+    document.querySelector("#add-card-12").style.display ="none";
+    document.querySelector(".list-group-12").style.display="none";
+}
+function overTT(){
+   
+    document.querySelector("#add-card-13").style.display ="block";
+ document.querySelector(".list-group-13").style.display="block";
+}
+   
+function outTT(){
+    document.querySelector("#add-card-13").style.display ="none";
+    document.querySelector(".list-group-13").style.display="none";
+}
+function overFT(){
+   
+    document.querySelector("#add-card-14").style.display ="block";
+ document.querySelector(".list-group-14").style.display="block";
+}
+   
+function outFT(){
+    document.querySelector("#add-card-14").style.display ="none";
+    document.querySelector(".list-group-14").style.display="none";
+}
+
+
+
+
+
+
